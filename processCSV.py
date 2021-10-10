8358cc2967c05fd6625e622d97030d8ac9490a11
Sawant Saurav <69086681+1498saurav@users.noreply.github.com>
2021-10-10 09:49:01 +0000

    Variable changed for data frames and print

--- processCSV.py
+++ processCSV.py
@@ -11,15 +11,15 @@ def processData():
 			filename=file.path
 
 		data=pd.read_csv(filename)
-		data=pd.pivot_table(data,index="State",columns="Severity",values="Due Date",aggfunc=len,fill_value=0)
-		data["Grand Total"] = data.sum(axis=1)
-		data.loc["Grand Total"] = data.select_dtypes(pd.np.number).sum()
-
-		#data=data.crosstab(Sever, rownames=['a'], colnames=['b', 'c'])
+		data_pivot=pd.pivot_table(data,index="State",columns="Severity",values="Due Date",aggfunc=len,fill_value=0)
+		data_pivot["Grand Total"] = data_pivot.sum(axis=1)
+		data_pivot.loc["Grand Total"] = data_pivot.select_dtypes(pd.np.number).sum()
+		
+		#data_pivot=data_pivot.crosstab(Sever, rownames=['a'], colnames=['b', 'c'])
 		#pd.crosstab(a, [b, c], rownames=['a'], colnames=['b', 'c'])
-		print("Processing Data!")
-		print(data)
-		return data
+		print("Processing data_pivot!")
+		print(data_pivot)
+		return data_pivot
 		
 	except:
 		print("Processing Failed!")
