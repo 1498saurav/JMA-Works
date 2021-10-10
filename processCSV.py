9c206301e825d9ce07fa4f0f753d85565b81c221
Sawant Saurav <69086681+1498saurav@users.noreply.github.com>
2021-10-10 14:42:46 +0000

    Filed Against Retrieved From Data Set

--- processCSV.py
+++ processCSV.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 
 def processData():
+	filedAgainst=""
 	try:
 		filename=""
 		data_store = []
@@ -13,15 +14,22 @@ def processData():
 		data=pd.read_csv(filename)
 		data_pivot=pd.pivot_table(data,index="State",columns="Severity",values="Due Date",aggfunc=len,fill_value=0)
 		data_pivot["Grand Total"] = data_pivot.sum(axis=1)
-		data_pivot.loc["Grand Total"] = data_pivot.select_dtypes(pd.np.number).sum()
+		data_pivot.loc["Grand Total"] = data_pivot.select_dtypes(np.number).sum()
 		data_store.append(data_pivot)
+		
+		df=data['Filed Against'].unique()
+		df = pd.DataFrame(data=df)
+		df=df.dropna()
+		#df[0].to_string(index=False)
+		filedAgainst=(df[0].to_list())
+		print(filedAgainst)
 		
 		test=data_pivot
 		for i in ["JioPages Mobility","Jio Saavn"]:
 			test=data[data['Filed Against'] == i]
 			test=pd.pivot_table(test,index="State",columns="Severity",values="Due Date",aggfunc=len,fill_value=0)
 			test["Grand Total"] = test.sum(axis=1)
-			test.loc["Grand Total"] = test.select_dtypes(pd.np.number).sum()
+			test.loc["Grand Total"] = test.select_dtypes(np.number).sum()
 			data_store.append(test)
 
 		#data_pivot=data_pivot.crosstab(Sever, rownames=['a'], colnames=['b', 'c'])
